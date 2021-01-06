45ca91a273204fca3a6d128ff111e969b05f10d1
Tyler Richey <iam@tylerrichey.com>
2021-01-06 09:51:44 -0800

    missing file

---
+++ Tiles/FeedHandler/FeedHandlerItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tiles.FeedHandler
+{
+    public class FeedHandlerItem
+    {
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string FeedName { get; set; }
+    }
+}
